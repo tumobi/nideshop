8dfb854d6065d3a76d57590c59f13148c4f11d89
Kit <kit_21cn@21cn.com>
2018-03-21 18:48:04 +0800

    商家列表按排序号进行排序

--- src/api/controller/brand.js
+++ src/api/controller/brand.js
@@ -3,7 +3,7 @@ const Base = require('./base.js');
 module.exports = class extends Base {
   async listAction() {
     const model = this.model('brand');
-    const data = await model.field(['id', 'name', 'floor_price', 'app_list_pic_url']).page(this.get('page') || 1, this.get('size') || 10).countSelect();
+    const data = await model.field(['id', 'name', 'floor_price', 'app_list_pic_url']).order({id: 'asc', sort_order: 'asc'}).page(this.get('page') || 1, this.get('size') || 10).countSelect();
 
     return this.success(data);
   }
